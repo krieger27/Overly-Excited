2b3b6f7ad028d6cd130438738005e27665b9b7b0
krieger27 <krieger@vols.utk.edu>
2017-12-05 14:58:53 -0600

    final

---
+++ overly-excited.js
@@ -0,0 +1,14 @@
+var sentence =["I ", "Love ", "Chickfila ", "so ", "much."]
+
+var newOne ="";
+
+ function addExcitement (theWordArray) {
+    for (i=0; i<theWordArray.length; i++){
+        newOne += "";
+        newOne += sentence[i];
+    }
+    console.log(newOne);
+ }
+     
+ 
+ addExcitement(sentence)
